94724d29b5ff7d42011decaf1d93369cbc35271e
student <student@nw42703.nwmissouri.edu>
2018-03-30 09:40:20 -0500

    Karthik - Stable version

--- ChildrenMonitoringSystem/ChildrenMonitoringSystem/ChangePasswordViewController.swift
+++ ChildrenMonitoringSystem/ChildrenMonitoringSystem/ChangePasswordViewController.swift
@@ -39,29 +39,35 @@ class ChangePasswordViewController: UIViewController {
     }
     @IBAction func changePasswodBTN(_ sender: UIButton) {
         
-//        if let cPass = currentPassTF.text , let nPass = newPassTF.text , let rPass = rePassTF.text{
-//            if(cPass == AppDelegate.password && nPass == rPass){
-//                Types.tryblock({ () -> Void in
-//                    let registeredUser = AppDelegate.DBInstance.backendless.userService.currentUser
-//                    print("User has been registered (SYNC): \(String(describing: registeredUser))")
-//
-//                    let properties = [
-//                        "password" : nPass
-//                    ]
-//
-//                    registeredUser?.updateProperties(properties)
-//
-//                    let updatedUser = AppDelegate.DBInstance.backendless.userService.update(registeredUser)
+        if let cPass = currentPassTF.text , let nPass = newPassTF.text , let rPass = rePassTF.text{
+            if(cPass == AppDelegate.password && nPass == rPass){
+                Types.tryblock({ () -> Void in
+                    let registeredUser = AppDelegate.DBInstance.backendless.userService.currentUser
+                    print("User has been registered (SYNC): \(String(describing: registeredUser))")
+
+                    let properties = [
+                        "password" : nPass
+                    ]
+
+                    registeredUser?.updateProperties(properties)
+Types.tryblock({ () -> Void in
+    _ = AppDelegate.DBInstance.backendless.userService.update(registeredUser)
+    
+}, catchblock: {(exception) ->Void in
+    
+    print("In the exception")
+})
+                    
 //                    print("User updated (SYNC): \(String(describing: updatedUser))")
 //
-//                },catchblock: { (exception) -> Void in
-//                    print("Server reported an error: \(String(describing: exception))" )
-//                })
-//            }
-//
-//        }
+                },catchblock: { (exception) -> Void in
+                    print("Server reported an error: \(String(describing: exception))" )
+                })
+            }
+
+        }
 //        print("out of if")
-        self.performSegue(withIdentifier: "unwindSegue", sender: self)
+     self.performSegue(withIdentifier: "unwindSegue", sender: self)
         
     }
     
